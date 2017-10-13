6e4271707505c2c85a9ccafc7af4246b8409dee6
Jose Tejada <Jose Tejada@Jos>
2017-10-13 08:28:56 -0600

    Cambios para probar Ejercicio 1 (Alarma parte 1)

--- Laboratorio 4/src/Alarma.java
+++ Laboratorio 4/src/Alarma.java
@@ -3,11 +3,18 @@ public class Alarma extends Timbre {
 		System.out.println("alarma 1");
 	}
 	 
-	public void m2() {
+	/*public void m2() {
 		super.m1();
 	}
 	 
 	public String toString() {
 		return super.toString() + "…" + super.toString();
 	}
+        */
+        public static void main(String[] args) {
+   Alarma miReloj = new Alarma(); 
+   System.out.println(miReloj); 
+   miReloj.m1(); 
+   miReloj.m2();
+}
 }
--- Laboratorio 4/src/Timbre.java
+++ Laboratorio 4/src/Timbre.java
@@ -2,11 +2,16 @@ public class Timbre {
 	public void m1() {
 		System.out.println("timbre 1");
 	}
-
+        /**
+         * Este metodo imprime el timbre dos 
+         */
 	public void m2() {
 		System.out.println("timbre 2");
 	}
-
+        /**
+         * ESte metodo es un toString d ela clase padre
+         * @return un RING RINGs
+         */
 	public String toString() {
 		return "Ring…Ring";
 	}

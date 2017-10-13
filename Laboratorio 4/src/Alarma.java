78b94c6ce8a200a62af680d80451f8406ff1db41
Jose Tejada <Jose Tejada@Jos>
2017-10-13 08:40:51 -0600

    Pruebas Para Alarma y Timbre 2 Ronda (Ejercicio 2)

--- Laboratorio 4/src/Alarma.java
+++ Laboratorio 4/src/Alarma.java
@@ -1,16 +1,24 @@
 public class Alarma extends Timbre {
+        /**
+         * Este metodo imprime alarma 1 porque se modificó el metodo original.
+         */
 	public void m1() {
 		System.out.println("alarma 1");
 	}
-	 
-	/*public void m2() {
+	/**
+         * Metodo que hereda al metodo m1 del padre
+         */
+	public void m2() {
 		super.m1();
 	}
-	 
+	/**
+         * Método personalizado de la clase Alarma, este metodo personaliza el .toString del padre
+         * @return un String con Ring Ring de Alarma y el RIng Ring del apdre
+         */
 	public String toString() {
 		return super.toString() + "…" + super.toString();
 	}
-        */
+        
         public static void main(String[] args) {
    Alarma miReloj = new Alarma(); 
    System.out.println(miReloj); 
--- Laboratorio 4/src/Timbre.java
+++ Laboratorio 4/src/Timbre.java
@@ -1,12 +1,13 @@
 public class Timbre {
 	public void m1() {
-		System.out.println("timbre 1");
+		System.out.println("Timbre 1");
 	}
         /**
          * Este metodo imprime el timbre dos 
          */
 	public void m2() {
-		System.out.println("timbre 2");
+            
+		System.out.println("Timbre 2");
 	}
         /**
          * ESte metodo es un toString d ela clase padre
